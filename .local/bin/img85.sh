69a3f7e65d09863c89fb7af550df4e04a73e67c4
anrawines <anrawines@gmail.com>
2026-03-24 16:36:00 +0700

    Update: 2026-03-24 16:36

---
+++ .local/bin/img85.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for file in "$@"; do
+    if [[ "$file" =~ \.(jpg|jpeg|png|webp|bmp)$ ]]; then
+        echo "Compressing: $(basename "$file")"
+        magick "$file" -quality 85 "${file%.*}_compressed.jpg"
+    fi
+done
+notify-send "Compression Complete" "Processed $# images"
